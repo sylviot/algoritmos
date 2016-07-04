661f3dcf0cbcd22904682979ee0b831d38c9a497
sylviot <sylvio.tavares@hotmail.com>
2016-07-04 08:57:35 -0300

    Ajuste basic condition

--- basic/Condition.cpp
+++ basic/Condition.cpp
@@ -4,30 +4,33 @@ using namespace std;
 
 int main(){
 
-	if(true){
+	/*		IF 		*/
 
+	// IF simples (Apenas exemplo)
+	if(true){ /* Condição booleana */
+		// Se a condição for verdadeira.
 	}
 
+	// Multiplos IFs (Apenas exemplo)
 	if(true){
-
 	}
 	else if(true){
-
 	}
 
+	// Multiplos IFs com ELSE (Apenas exemplo)
 	if(true){
-
 	}
 	else if(true){
-
 	}
 	else{
-
+		// Caso nenhuma condição dos IFs for verdadeira
 	}
 
+	// IF ternário
 	int v_ou_f = (true ? 1 : 0);
 
 	int number = 1;
+	/*		SWITCH		*/
 	switch(number){
 		case 1:
 			printf("Number 1");
