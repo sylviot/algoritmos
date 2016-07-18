e3ac9d085123096192c30c50932eb7c28a7d7bab
Lucas Bernieri Ramos <lucasramos53@gmail.com>
2016-07-18 09:23:42 -0400

    Renomeando o arquivo de input do Python e adicionando comentários ao código

---
+++ basic/Input.py
@@ -0,0 +1,9 @@
+import fileinput
+
+class Input:
+
+    def main(self):
+        input_int = int(raw_input()) # Converte para inteiro
+        input_float = float(raw_input()) # Converte para ponto flutuante
+        input_str = str(raw_input()) # Converte para string
+        input_tuple = tuple(raw_input()) # Converte para tuple
