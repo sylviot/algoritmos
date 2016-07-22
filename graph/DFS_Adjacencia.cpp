910561e0988dc3d55342c1158f96ecf3c91a7ba8
sylviot <sylvio.tavares@hotmail.com>
2016-07-22 16:14:30 -0300

    DFS's

---
+++ graph/DFS_Adjacencia.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define MAX 10
+
+using namespace std;
+
+vector<int> G[MAX];
+bool V[MAX];
+
+int dfs(int v, int p)
+{
+	V[v] = true;
+
+	for(int w : G[v])
+	{
+		if(!V[w])
+		{
+			dfs(w, v);
+		}
+	}
+}
+
+int dfs(int v)
+{
+	memset(V, false, sizeof(V));
+
+	dfs(v, -1);
+}
+
+void addEdge(int v, int w)
+{
+	G[v].push_back(w);
+	G[w].push_back(v);
+}
+
+int main()
+{
+	/*
+	         1---3
+	        / \   \
+	       2---4   5
+            \ /    |
+             6     7
+	*/
+
+    addEdge(1, 2);
+    addEdge(1, 3);
+    addEdge(1, 4);
+    addEdge(2, 4);
+    addEdge(2, 6);
+    addEdge(3, 5);
+    addEdge(4, 6);
+    addEdge(5, 7);
+
+    dfs(1);
+
+	return 0;
+}
